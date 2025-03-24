ad75269c1c5a257c3c47942b341cef9b484a79fa
alex turcotte <altur112@ulaval.ca>
2025-03-24 13:11:13 -0400

    2

---
+++ equipe-22-main.py
@@ -0,0 +1,9 @@
+import numpy
+import sympy
+
+print("hello world")
+
+#imshow(matrice en arg)
+#double boucle pour moyenner les valeurs
+#chque fois qu'on remoyenne, on remet nos noeuds à potentiel constant, on remoyenne, on réasigne le potentiel constant
+#on fait ca une dizaine de fois, puis on affiche la matrice pour voir. jusqu'à un nombre de fois ou on voit que ca ne change presque plus
